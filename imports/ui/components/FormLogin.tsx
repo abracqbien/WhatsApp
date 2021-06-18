247818b00b0ea91813fbfaaa70ecd5a1b6cbb5a4
ABracqbien <abracqbien@laposte.net>
2021-06-18 16:53:04 +0200

    Add login state

--- imports/ui/components/FormLogin.tsx
+++ imports/ui/components/FormLogin.tsx
@@ -4,30 +4,57 @@ import React from 'react'
 import StyledFormLogin from '../elements/StyledFormLogin'
 
 const FormLogin = (props:any):JSX.Element => {
+  // State
+  const [state, setState] = React.useState<any>({
+    username: "",
+    password: "",
+    phone: "",
+  })
+  const { username, password, phone } = state
+
+  // Functions
+  const handleChange = (e:React.ChangeEvent<HTMLInputElement>):void => {
+    const inputValue:string = e.target.value
+    const inputName:string = e.target.name
+
+    setState(prevState => (
+      {
+        ...prevState,
+        [inputName]: inputValue
+      }
+    ))
+  }
+
   return (
     <StyledFormLogin>
       <label className="label">
         <input
-          type="text"
+          onChange={handleChange}
+          value={username}
+          placeholder="Nom d'Utilisateur"
           className="input"
           name="username"
-          placeholder="Nom d'Utilisateur"
+          type="text"
         />
       </label>
       <label className="label">
         <input
-          type="text"
+          onChange={handleChange}
+          value={phone}
+          placeholder="Téléphone"
           className="input"
           name="phone"
-          placeholder="Téléphone"
+          type="text"
         />
       </label>
       <label className="label">
         <input
-          type="password"
-          className="input"
-          name="password"
+          onChange={handleChange}
+          value={password}
           placeholder="Mot de passe"
+          className="input"
+          type="password"
+          name="password"
         />
       </label>
       <button className="loginBtn">CONNEXION</button>
